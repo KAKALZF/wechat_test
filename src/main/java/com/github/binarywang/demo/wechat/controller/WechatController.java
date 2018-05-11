c8c04902126993e924b927248392eca54a826d64
linzefeng <57580053@qq.com>
2018-05-11 17:36:22 +0800

    dev

--- src/main/java/com/github/binarywang/demo/wechat/controller/WechatController.java
+++ src/main/java/com/github/binarywang/demo/wechat/controller/WechatController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/wechat/portal")
 public class WechatController {
+  //日志
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @Autowired
