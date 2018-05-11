b6472d8afd7f74dc7cfbee16278b11fcb7a1e0f8
linzefeng <57580053@qq.com>
2018-05-11 17:28:31 +0800

    dw

--- src/main/java/com/github/binarywang/demo/wechat/builder/AbstractBuilder.java
+++ src/main/java/com/github/binarywang/demo/wechat/builder/AbstractBuilder.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
  * @author Binary Wang(https://github.com/binarywang)
  */
 public abstract class AbstractBuilder {
+
   protected final Logger logger = LoggerFactory.getLogger(getClass());
 
   public abstract WxMpXmlOutMessage build(String content,
